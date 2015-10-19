210393e742e998e8d50848119bb6891a5a7b15db
Eben <me@ebenroux.co.za>
2015-10-19 14:25:43 +0200

    - removed IDbConnectionConfiguration

--- Shuttle.Core.Data/IDbConnectionConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace Shuttle.Core.Data
-{
-    public interface IDbConnectionConfiguration
-    {
-        string Name { get; }
-        string ProviderName { get; }
-        string ConnectionString { get; }
-    }
-}
