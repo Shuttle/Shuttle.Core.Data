8c51d9328c3c8640c7c66c332cb2db99c23e1ff8
Eben <me@ebenroux.co.za>
2016-01-01 08:15:05 +0200

    - v3.2.10

--- Shuttle.Core.Data/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Data/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.Data for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("3.2.9.0")]
+[assembly: AssemblyVersion("3.2.10.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.2.9")]
+[assembly: AssemblyInformationalVersion("3.2.10")]
 [assembly: ComVisible(false)]
