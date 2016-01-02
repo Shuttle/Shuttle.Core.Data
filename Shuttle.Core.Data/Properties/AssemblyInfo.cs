12bd7e80f6dc2b45026f2d58f233f8b73383e2db
Eben <me@ebenroux.co.za>
2016-01-02 16:54:41 +0200

    - updated year

--- Shuttle.Core.Data/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Data/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyVersion("3.2.10.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
