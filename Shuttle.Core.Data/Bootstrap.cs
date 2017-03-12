4d64cd8d5a715db15f28bf1d5a0f3bfd35f9b87a
Eben <me@ebenroux.co.za>
2017-03-12 20:17:53 +0200

    - bootstrap

---
+++ Shuttle.Core.Data/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Core.Data
+{
+	public class Bootstrap : IComponentRegistryBootstrap
+	{
+		public void Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			registry.AttemptRegister<IScriptProviderConfiguration, ScriptProviderConfiguration>();
+			registry.AttemptRegister<IScriptProvider, ScriptProvider>();
+
+			registry.AttemptRegister<IDatabaseContextCache, ThreadStaticDatabaseContextCache>();
+			registry.AttemptRegister<IDatabaseContextFactory, DatabaseContextFactory>();
+			registry.AttemptRegister<IDbConnectionFactory, DbConnectionFactory>();
+			registry.AttemptRegister<IDbCommandFactory, DbCommandFactory>();
+			registry.AttemptRegister<IDatabaseGateway, DatabaseGateway>();
+			registry.AttemptRegister<IQueryMapper, QueryMapper>();
+		}
+	}
+}
--- Shuttle.Core.Data/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Data/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.Data for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("4.2.11.0")]
+[assembly: AssemblyVersion("4.2.13.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("4.2.11")]
+[assembly: AssemblyInformationalVersion("4.2.13")]
 [assembly: ComVisible(false)]
