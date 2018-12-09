beeb42218ed9f8c60178b01ea408dd38f8843f78
eben-roux <me@ebenroux.co.za>
2018-12-09 08:34:00 +0200

    - ActiveDatabaseContext

---
+++ Shuttle.Core.Data/ActiveDatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Core.Data
+{
+    public class ActiveDatabaseContext : IDisposable
+    {
+        private readonly IDatabaseContextCache _databaseContextCache;
+        private readonly IDatabaseContext _current;
+
+        public ActiveDatabaseContext(IDatabaseContextCache databaseContextCache, IDatabaseContext current)
+        {
+            Guard.AgainstNull(databaseContextCache, nameof(databaseContextCache));
+
+            _databaseContextCache = databaseContextCache;
+            _current = current;
+        }
+
+        public void Dispose()
+        {
+            if (_current == null)
+            {
+                return;
+            }
+
+            _databaseContextCache.Use(_current);
+        }
+    }
+}
