1b933626b9654f5cc90735d2627ffe1c6f7aa512
Eben <me@ebenroux.co.za>
2024-03-02 17:29:29 +0200

    - moved static SectionName to options

--- Shuttle.Core.Data/Configuration/DataAccessOptions.cs
+++ Shuttle.Core.Data/Configuration/DataAccessOptions.cs
@@ -2,6 +2,8 @@ namespace Shuttle.Core.Data
 {
     public class DataAccessOptions
     {
+        public const string SectionName = "Shuttle:DataAccess";
+        
         public int CommandTimeout { get; set; } = 15;
         public DatabaseContextFactoryOptions DatabaseContextFactory { get; set; } = new DatabaseContextFactoryOptions();
     }
